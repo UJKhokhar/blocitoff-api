ef47a0cc07ce128bc6a527ce007f5f119b387a94
ujkhokhar <ujkhokhar@gmail.com>
2015-03-25 17:43:54 -0400

    Add items#create and items#show.

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,6 +3,19 @@ class Api::ItemsController < Api::ApiController
     user = User.find(params[:user_id])
     items = user.items
 
-    render json: items
+    render json: items 
+  end
+
+  def show
+    item = Item.find(params[:id])  
+
+    render json: item
+  end
+
+  def create
+    item = Item.create(params.require(:item).permit(:name))
+    if item.save  
+      render json: item
+    end
   end
 end
