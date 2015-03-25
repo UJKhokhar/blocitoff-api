ba1a236d5646a6b162f6dd827686a13b3f9405cb
ujkhokhar <ujkhokhar@gmail.com>
2015-03-25 18:02:30 -0400

    Add items#destroy.

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -14,8 +14,17 @@ class Api::ItemsController < Api::ApiController
 
   def create
     item = Item.create(params.require(:item).permit(:name))
+    creation_confirmation = "Item created."
     if item.save  
       render json: item
     end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    destroy_confirmation = "Item destroyed."
+
+    render json: destroy_confirmation
   end
 end
