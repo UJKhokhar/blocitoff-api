e6221916eaf81382040672d7ebeb00f9fa488663
ujkhokhar <ujkhokhar@gmail.com>
2015-03-27 22:29:38 -0400

    Add authentication token to users upon creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
+require 'securerandom'
+
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_create :set_auth_token
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :items
+
+  private 
+    def set_auth_token
+      return if auth_token.present?
+      self.auth_token = generated_auth_token
+    end
+
+    def generate_auth_token
+      SecureRandom.uuid.gsub(/\-/, '')
+    end
 end
