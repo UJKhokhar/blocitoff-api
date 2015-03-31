fde719e885433401ec1805be6c6fd698355eebb4
ujkhokhar <ujkhokhar@gmail.com>
2015-03-31 04:43:17 -0400

    Add user_id to lists. Add list_id to items.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331031259) do
+ActiveRecord::Schema.define(version: 20150331083945) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "list_id"
   end
 
+  add_index "items", ["list_id"], name: "index_items_on_list_id"
   add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "lists", force: :cascade do |t|
@@ -28,7 +30,10 @@ ActiveRecord::Schema.define(version: 20150331031259) do
     t.string   "permission",  default: "private"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.integer  "user_id"
   end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
