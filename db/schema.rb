3b8f8c0d4e243a5db2710f87e52257dd09422c58
ujkhokhar <ujkhokhar@gmail.com>
2015-03-30 23:35:41 -0400

    Add lists model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330065549) do
+ActiveRecord::Schema.define(version: 20150331031259) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150330065549) do
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "permission",  default: "private"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
