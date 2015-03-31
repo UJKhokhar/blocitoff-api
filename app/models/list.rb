b9fe89c74399d7033cb3345f9a71b24301302f1f
ujkhokhar <ujkhokhar@gmail.com>
2015-03-31 00:40:41 -0400

    Add validation to list permissions.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
+  validates :permission, inclusion: { in: %w(private open viewable), 
+    message: "%{value} is not a valid permission"}
 end
