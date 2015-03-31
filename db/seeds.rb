84744baad3a64411f1bcaf3bbbd352c5edf15d9a
Jean-Denis Vauguet <jd@vauguet.fr>
2015-03-31 22:57:40 +0200

    Fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 # Create Users
 5.times do
-  user = User.create!(
+  user = User.new(
     name: Faker::Name.name,
     email: Faker::Internet.email,
     password: Faker::Lorem.characters(10)
@@ -11,17 +11,17 @@ require 'faker'
   user.save!
 end
 
-umar = User.create!(
+# Special user
+umar = User.new(
   name: 'Umar Khokhar',
   email: 'ujkhokhar@gmail.com',
   password: 'helloworld'
 )
-
 umar.skip_confirmation!
 umar.save!
 
 users = User.all
-puts "#{User.count} users created"
+puts "6 users created"
 
 # Create items
 25.times do
@@ -30,7 +30,6 @@ puts "#{User.count} users created"
     name: Faker::Lorem.sentence
   )
 end
-items = Item.all
-puts "#{Item.count} items created"
+puts "25 items created"
 
 puts "Seed finished"
