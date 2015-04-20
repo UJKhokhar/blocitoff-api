2031687366082506f8350145439754bcdcd5f4ca
ujkhokhar <ujkhokhar@gmail.com>
2015-04-20 16:59:51 -0400

    Return 200 status on user creation.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,16 +8,16 @@ class Api::UsersController < Api::ApiController
   end
 
   def create
-    user = User.build(user_params)
-    user.save
-
-    render json: user
+    user = User.new(user_params)
+    if user.save  
+      head :ok
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:id, :auth_token)
+    params.require(:user).permit(:auth_token)
   end
 
 end
