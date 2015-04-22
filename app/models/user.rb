90d13ae43e48d440ef2abc03126ae5fd29199afd
ujkhokhar <ujkhokhar@gmail.com>
2015-04-21 22:34:09 -0400

    Remove authentication token creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,4 @@
-require 'securerandom'
-
 class User < ActiveRecord::Base
   has_many :lists
   has_many :items
-
-  after_create :set_auth_token
-
-  protected
-
-  def set_auth_token
-    auth_token = nil
-    begin
-      auth_token = SecureRandom.uuid.gsub(/\-/, '')
-    end while self.class.exists?(auth_token: auth_token)
-    self.update_attribute :auth_token, auth_token
-  end
 end
